eb176399685ba08b24bd074fa457caafc92df4a3
delphes1 <delphes5@outlook.fr>
2019-12-19 09:09:58 +0100

    Démarrage

---
+++ dark-place-game/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace dark_place_game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lancement du jeu Dark Place ...");
+            throw new Exception("Le jeu ne semble pas encore codé !");
+        }
+    }
+}
