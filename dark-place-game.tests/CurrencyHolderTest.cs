459efe266aa4e7713ff70aba50e0c374d3c77369
delphes1 <delphes5@outlook.fr>
2019-12-19 14:56:23 +0100

    Dernière version

--- dark-place-game.tests/CurrencyHolderTest.cs
+++ dark-place-game.tests/CurrencyHolderTest.cs
@@ -59,38 +59,150 @@ namespace dark_place_game.tests
             Action mauvaisAppel = () => new CurrencyHolder("", EXEMPLE_CAPACITE_VALIDE, EXEMPLE_CONTENANCE_INITIALE_VALIDE);
             Assert.Throws<ArgumentException>(mauvaisAppel);
         }
-        /** #TODO_ETAPE_4
-[Fact]
-public void BrouzoufIsAValidCurrencyName ()
-{
-// A vous d'écrire un test qui vérife qu'on peut créer un CurrencyHolder contenant une monnaie dont le nom est Brouzouf
-}
-[Fact]
-public void DollardIsAValidCurrencyName ()
-{
-// A vous d'écrire un test qui vérife qu'on peut créer un CurrencyHolder contenant une monnaie dont le nom est Dollard
-}
-[Fact]
-public void TestPut10CurrencyInNonFullCurrencyHolder()
-{
-// A vous d'écrire un test qui vérifie que si on ajoute via la methode put 10 currency à un sac a moité plein, il contient maintenant la bonne quantité de currency
-}
-[Fact]
-public void TestPut10CurrencyInNearlyFullCurrencyHolder()
-{
-// A vous d'écrire un test qui vérifie que si on ajoute via la methode put 10 currency à un sac quasiement plein, une exeption NotEnoughtSpaceInCurrencyHolderExeption est levée.
-}
-[Fact]
-public void CreatingCurrencyHolderWithNameShorterThan4CharacterThrowExeption()
-{
-// A vous d'écrire un test qui doit échouer s'il es possible de créer un CurrencyHolder dont Le Nom De monnaie est inférieur a 4 lettres
-}
-[Fact]
-public void WithdrawMoreThanCurrentAmountInCurrencyHolderThrowExeption()
-{
-// A vous d'écrire un test qui vérifie que retirer (methode withdraw) une quantité negative de currency leve une exeption CantWithDrawNegativeCurrencyAmountExeption.
-// Asruce : dans ce cas prévu avant même de pouvoir compiler le test, vous allez être obligé de créer la classe CantWithDrawMoreThanCurrentAmountExeption (vous pouvez la mettre dans le meme fichier que CurrencyHolder)
-}
-#TODO_ETAPE_4 */
+        [Fact]
+        public void CreatingCurrencyHolderWithBigNameThrowExeption()
+        {
+            // Test si un nom est supérieur à 10 charactères
+            Action mauvaisAppel = () => new CurrencyHolder("jesuisunlongname", EXEMPLE_CAPACITE_VALIDE, EXEMPLE_CONTENANCE_INITIALE_VALIDE);
+            Assert.Throws<ArgumentException>(mauvaisAppel);
+        }
+        [Fact]
+        public void BrouzoufIsAValidCurrencyName()
+        {
+            // A vous d'écrire un test qui vérife qu'on peut créer un CurrencyHolder contenant une monnaie dont le nom est Brouzouf
+            CurrencyHolder currency = new CurrencyHolder("Brouzouf",EXEMPLE_CAPACITE_VALIDE ,EXEMPLE_CONTENANCE_INITIALE_VALIDE);
+
+        }
+        [Fact]
+        public void DollardIsAValidCurrencyName()
+        {
+            // A vous d'écrire un test qui vérife qu'on peut créer un CurrencyHolder contenant une monnaie dont le nom est Dollard
+            CurrencyHolder currency = new CurrencyHolder("Dollard",EXEMPLE_CAPACITE_VALIDE ,EXEMPLE_CONTENANCE_INITIALE_VALIDE);
+        }
+        [Fact]
+        public void TestPut10CurrencyInNonFullCurrencyHolder()
+        {
+            // A vous d'écrire un test qui vérifie que si on ajoute via la methode put 10 currency à un sac a moité plein, il contient maintenant la bonne quantité de currency
+            CurrencyHolder currency = new CurrencyHolder(EXEMPLE_NOM_MONNAIE_VALIDE, EXEMPLE_CAPACITE_VALIDE/2, EXEMPLE_CONTENANCE_INITIALE_VALIDE);
+            currency.Put(10);
+            Assert.True(currency.CurrentAmount == EXEMPLE_CONTENANCE_INITIALE_VALIDE + 10);
+        }
+        [Fact]
+        public void TestPut10CurrencyInNearlyFullCurrencyHolder()
+        {
+            // A vous d'écrire un test qui vérifie que si on ajoute via la methode put 10 currency à un sac quasiement plein, une exeption NotEnoughtSpaceInCurrencyHolderExeption est levée.
+            
+            Action mauvaisAppel = () => {
+                CurrencyHolder currency = new CurrencyHolder(EXEMPLE_NOM_MONNAIE_VALIDE,10,9);
+                currency.Put(10);
+            };
+            
+            Assert.Throws<NotEnoughtSpaceInCurrencyHolderExeption>(mauvaisAppel);
+
+        }
+        [Fact]
+        public void CreatingCurrencyHolderWithNameShorterThan4CharacterThrowExeption()
+        {
+            // A vous d'écrire un test qui doit échouer s'il est possible de créer un CurrencyHolder dont Le Nom De monnaie est inférieur a 4 lettres
+            Action mauvaisAppel = () => new CurrencyHolder("del", EXEMPLE_CAPACITE_VALIDE, EXEMPLE_CONTENANCE_INITIALE_VALIDE);
+            Assert.Throws<ArgumentException>(mauvaisAppel);
+        }
+        [Fact]
+        public void WithdrawMoreThanCurrentAmountInCurrencyHolderThrowExeption()
+        {
+            // A vous d'écrire un test qui vérifie que retirer (methode withdraw) une quantité negative de currency leve une exeption CantWithDrawNegativeCurrencyAmountExeption.
+            // Asruce : dans ce cas prévu avant même de pouvoir compiler le test, vous allez être obligé de créer la classe CantWithDrawMoreThanCurrentAmountExeption (vous pouvez la mettre dans le meme fichier que CurrencyHolder)
+            Action mauvaisAppel = () => {
+                CurrencyHolder currency = new CurrencyHolder(EXEMPLE_NOM_MONNAIE_VALIDE,10,9);
+                currency.Withdraw(-5);
+            };
+            
+            Assert.Throws<CantWithDrawNegativeCurrencyAmountExeption>(mauvaisAppel);
+        }
+        [Fact]
+        public void PutNegativeCurrentAmountInCurrencyHolderThrowExeption(){
+            Action mauvaisAppel = () => {
+                CurrencyHolder currency = new CurrencyHolder(EXEMPLE_NOM_MONNAIE_VALIDE,10,9);
+                currency.Put(-5);
+            };
+            
+            Assert.Throws<CantPutNegativeCurrencyAmountExeption>(mauvaisAppel);
+        }
+        [Fact]
+        public void PutNullCurrencyAmountExeption(){
+            Action mauvaisAppel = () => {
+                CurrencyHolder currency = new CurrencyHolder(EXEMPLE_NOM_MONNAIE_VALIDE,10,9);
+                currency.Put(0);
+            };
+            
+            Assert.Throws<CantPutNullCurrencyAmountExeption>(mauvaisAppel);  
+        }
+        [Fact]
+        public void WithDrawNullCurrencyAmountExeption(){
+            Action mauvaisAppel = () => {
+                CurrencyHolder currency = new CurrencyHolder(EXEMPLE_NOM_MONNAIE_VALIDE,10,9);
+                currency.Withdraw(0);
+            };
+            
+            Assert.Throws<CantWithDrawNullCurrencyAmountExeption>(mauvaisAppel);  
+        }
+        [Fact]
+        public void IfStartACurrencyAmountException(){
+            Action mauvaisAppel = () => new CurrencyHolder("acolyte", EXEMPLE_CAPACITE_VALIDE, EXEMPLE_CONTENANCE_INITIALE_VALIDE);
+            Assert.Throws<ArgumentException>(mauvaisAppel);
+        }
+        [Fact]
+        public void IfStartaCurrencyAmountException(){
+            Action mauvaisAppel = () => new CurrencyHolder("Acolyte", EXEMPLE_CAPACITE_VALIDE, EXEMPLE_CONTENANCE_INITIALE_VALIDE);
+            Assert.Throws<ArgumentException>(mauvaisAppel);
+        }
+        [Fact]
+        public void CreatingCurrencyHolderWithLessThanOneCapacityThrowExeption(){
+            Action mauvaisAppel = () => new CurrencyHolder("Exemple", 0, EXEMPLE_CONTENANCE_INITIALE_VALIDE);
+            Assert.Throws<ArgumentException>(mauvaisAppel);
+        }
+        [Fact]
+        public void CreatingCurrencyHolderWithMinimumOneCapacityThrowExeption(){
+            CurrencyHolder currency = new CurrencyHolder("Exemple",1,EXEMPLE_CONTENANCE_INITIALE_VALIDE);
+        }
+        [Fact]
+        public void CheckisNotEmpty(){
+            CurrencyHolder currency = new CurrencyHolder("Exemple",1,0);
+            currency.Put(1);
+            var result = currency.IsEmpty();
+            Assert.False(result);
+        }
+        [Fact]
+        public void CheckisEmpty(){
+            CurrencyHolder currency = new CurrencyHolder("Exemple",1,0);
+            var result = currency.IsEmpty();
+            Assert.True(result);
+        }
+        [Fact]
+        public void CheckisFull(){
+            CurrencyHolder currency = new CurrencyHolder("Exemple",1,0);
+            currency.Put(1);
+            var result = currency.IsFull();
+            Assert.True(result);
+        }
+        [Fact]
+        public void CheckisNotFull(){
+            CurrencyHolder currency = new CurrencyHolder("Exemple",2,0);
+            currency.Put(1);
+            var result = currency.IsFull();
+            Assert.False(result);
+        }
+        [Fact]
+        public void CheckisFullInitial(){
+            CurrencyHolder currency = new CurrencyHolder("Exemple",2,2);
+            var result = currency.IsFull();
+            Assert.True(result);
+        }
+        [Fact]
+        public void CheckisNotFullInitial(){
+            CurrencyHolder currency = new CurrencyHolder("Exemple",2,0);
+            var result = currency.IsFull();
+            Assert.False(result);
+        }
     }
 }
--- dark-place-game/CurrencyHolder.cs
+++ dark-place-game/CurrencyHolder.cs
@@ -4,6 +4,10 @@ namespace dark_place_game
     [System.Serializable]
     /// Une Exeption Custom
     public class NotEnoughtSpaceInCurrencyHolderExeption : System.Exception { }
+    public class CantWithDrawNegativeCurrencyAmountExeption : System.Exception { }
+    public class CantPutNegativeCurrencyAmountExeption : System.Exception { }
+    public class CantPutNullCurrencyAmountExeption : System.Exception { }
+    public class CantWithDrawNullCurrencyAmountExeption : System.Exception { }
     public class CurrencyHolder
     {
         public static readonly string CURRENCY_DEFAULT_NAME = "Unnamed";
@@ -42,30 +46,75 @@ namespace dark_place_game
         private int capacity = 0;
         public CurrencyHolder(string name, int capacity, int amount)
         {
+
             if(name == null){
                 throw new System.ArgumentException("Parameter cannot be null", "name");
             }else if(name == ""){
                 throw new System.ArgumentException("Parameter cannot be empty", "name");
+            }else if(name.Substring(0, 1).Equals("a") || name.Substring(0, 1).Equals("A")){
+                throw new System.ArgumentException("Parameter cannot be a or A");
             }else if(amount < 0){
                 throw new System.ArgumentException("Parameter cannot be negative", "amount");
+            }else if(name.Length > 10){
+                throw new System.ArgumentException("Parameter cannot be more than 10 characters", "name");
+            }else if(name.Length < 4){
+                throw new System.ArgumentException("Parameter cannot be less than 4 characters", "name");
+            }else if(capacity < 1){
+                throw new System.ArgumentException("Parameter cannot be less than 1", "capacity");
             }
+            
             Capacity = capacity;
             CurrencyName = name;
             CurrentAmount = amount;
         }
         public bool IsEmpty()
         {
+            if(CurrentAmount == 0){
             return true;
+            }else{
+                return false;
+            }
         }
         public bool IsFull()
-        {
-            return true;
+        {   
+            if(CurrentAmount == Capacity){
+                return true;
+            }else{
+                return false;
+            }
+            
         }
         public void Store(int amount)
         {
+            if(currentAmount + amount > Capacity){
+                throw new NotEnoughtSpaceInCurrencyHolderExeption();
+            }else{
+                currentAmount = currentAmount + amount;
+            }  
+        }
+        public void Put (int amount)
+        {   if(amount < 0){
+                throw new CantPutNegativeCurrencyAmountExeption();
+            }else if(amount == 0){
+                    throw new CantPutNullCurrencyAmountExeption();
+                }else{
+                if(currentAmount + amount > Capacity){
+                    throw new NotEnoughtSpaceInCurrencyHolderExeption();
+                }else{
+                    currentAmount = currentAmount + amount;
+                }    
+            }
         }
         public void Withdraw(int amount)
         {
+            if(amount < 0){
+                throw new CantWithDrawNegativeCurrencyAmountExeption();
+            }else if(amount == 0){
+                throw new CantWithDrawNullCurrencyAmountExeption();
+                }else{
+                currentAmount = currentAmount - amount;
+            } 
+            
         }
     }
 }
